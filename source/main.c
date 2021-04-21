9ad44ea84f9471b926b62d26a8d3824d0ac7ace5
terryfu <terryfu33@gmail.com>
2021-04-21 11:08:28 -0600

    try to flickk button

--- source/main.c
+++ source/main.c
@@ -26,10 +26,14 @@ int main() {
         if(up == 1){
             drawStartButton(whiteStart);
             drawEndButton(blackQuit);
+            Wait(2000);
+            drawStartButton(blackStart);
         }
         else{
             drawStartButton(blackStart);
             drawEndButton(whiteQuit);
+            Wait(2000);
+            drawStartButton(blackQuit);
         }
        
         Read_Snes();
