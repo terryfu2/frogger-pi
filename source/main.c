62385c7d81ee479eb04fc67e4418379e6891833a
terryfu <terryfu33@gmail.com>
2021-04-21 22:15:36 -0600

    Revert "esting"
    This reverts commit e94a36919e70a1636b16f1e8bbb4e05f401b8e8e.

--- source/main.c
+++ source/main.c
@@ -5,7 +5,6 @@
 #include "buttons.h"
 
 
-
 #include <stdio.h>
 
 
@@ -32,8 +31,12 @@ int main() {
         drawGrid();
         drawFrog(getGameState());
         //if start is pressed, open the pause menu - if any other button is pressed while in pause, end game
-        checkPause();
-
+        if(getStart() == 1){
+            drawHalf();
+        }
+        else if(getCheck() == 1){
+            break;
+        }
         drawScreen();
     }
 
@@ -42,14 +45,4 @@ int main() {
     Wait(2000000);
 
     return 0;
-}
-
-void checkPause(){
-
-    while(getStart() == 1 && getCheck() == 0){
-        drawHalf();
-        readPause();
-        drawScreen();
-    }
-
 }
